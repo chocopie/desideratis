0b37fb581373f45ce5b222dd1e522e465d378c30
chocopie <chocopie@users.noreply.github.com>
2014-09-11 21:24:31 +0800

    moved poem file to config; added --startfrom override; callbacks work properly now; added help

--- index.js
+++ index.js
@@ -1,61 +1,69 @@
 var argv = require('minimist')(process.argv.slice(2));
 var fs = require('fs');
 var async = require('async');
-// var cron_job = require('cron').CronJob;
 var twitter = require('twitter');
 var config = require('./config');
-var twit = new twitter({
+
+var _TWIT = new twitter({
     consumer_key: config.twitter_api_key,
     consumer_secret: config.twitter_api_secret,
     access_token_key: config.twitter_access_token_key,
     access_token_secret: config.twitter_access_token_secret
 });
+var _BOOKMARK_FILE = './bookmark.dat';
+var _POEM_FILE = config.poem;
 
 // arguments
-var POEM_FILE = argv.poem || null;
-// var START_FROM = argv.startfrom || 0;
-var BOOKMARK_FILE = './bookmark.dat';
+var START_FROM = argv.startfrom !== undefined ? (argv.startfrom === true ? 0 : argv.startfrom) : null;
+var HELP = argv.help || null;
 
-// bomb out if requirements aren't met
-if (!POEM_FILE) {
-	console.log('--poem not specified');
+// help if asked
+if (HELP) {
+	console.log('--startfrom <number>','\t','(optional) Set bookmark to <number>. First line is 0. Will % at poem length. Empty --startfrom will reset to 0.');
 	process.exit();
 }
 
-var params_for_last_tweet = {
-	user_id: config.twitter_api_key,
-	count: 1,
-	include_entities: true	
-};
-
-// 1. read bookmark
-//		-- if no bookmark, start at 0
-// 2. read poem
-// 3. tweet poem at line bookmark
-// 4. note down new bookmark
+// 1. check bookmark & read poem
+// 2. grab bookmarked line
+// 3. update bookmark
+// 4. tweet line
 async.parallel([
 	checkBookmark,
-	function (done) { fs.readFile(POEM_FILE, 'utf8', done); }
+	function (done) { fs.readFile(_POEM_FILE, 'utf8', done); }
 ], function (err, results) {
-	var bookmark = results[0] ? (results[0].trim() === '' ? 0 : parseInt(results[0].trim())) : 0;
+	if (err) console.log(err);
+	var bookmark = results[0];
 	var poem = results[1].split('\n');
 	var line = poem[bookmark % poem.length];
 	var new_bookmark = (bookmark + 1) % poem.length;
-	fs.writeFile(BOOKMARK_FILE, new_bookmark+'\n', 'utf8', function(err) {
+	fs.writeFile(_BOOKMARK_FILE, new_bookmark+'\n', 'utf8', function(err) {
 		if (err) throw err;
-		// twit.updateStatus(line, function(data) {
-		// 	return 'tweeted line '+ line +' at '+data.created_at;
-	 //    });
-		console.log(line);
+		tweetMessage(line, function(data) {
+			console.log('tweeted line', bookmark, ':', data.text, 'at', data.created_at);
+	    });
 	});
 });
 
 function checkBookmark(callback) {
-	fs.exists(BOOKMARK_FILE, function (exists) {
-		if (exists) {
-			fs.readFile(BOOKMARK_FILE, 'utf8', callback);
-		} else {
-			callback();
-		}
-	});
-};
+	if (START_FROM === null) {
+		fs.exists(_BOOKMARK_FILE, function (exists) {
+			if (exists) {
+				fs.readFile(_BOOKMARK_FILE, 'utf8', function(err, data) {
+					if (data === null) {
+						callback(null, 0);
+					} else {
+						callback(null, parseInt(data.trim()));
+					}
+				});
+			} else {
+				callback(null, 0);
+			}
+		});		
+	} else {
+		callback(null, START_FROM);
+	}
+}
+
+function tweetMessage(message, callback) {
+	_TWIT.updateStatus(message, callback);
+}
