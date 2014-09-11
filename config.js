bbf6abc2b427dc49a2321a879903abd3590c8001
Sandy Lim <chocopie@users.noreply.github.com>
2014-09-11 21:25:34 +0800

    Added poem field

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
-	twitter_api_key: 'YOUR_API_KEY',
-    twitter_api_secret: 'YOUR_API_SECRET',
-    twitter_access_token_key: 'YOUR_ACCESS_TOKEN',
-    twitter_access_token_secret: 'YOUR_ACCESS_TOKEN_SECRET'
+  poem: 'PATH_TO_YOUR_POEM',
+  twitter_api_key: 'YOUR_API_KEY',
+  twitter_api_secret: 'YOUR_API_SECRET',
+  twitter_access_token_key: 'YOUR_ACCESS_TOKEN',
+  twitter_access_token_secret: 'YOUR_ACCESS_TOKEN_SECRET'
 }
