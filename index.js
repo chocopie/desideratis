6aa1abe1ccc4a75e9d8711a7c8af81306e87731c
chocopie <chocopie@users.noreply.github.com>
2014-09-11 21:25:58 +0800

    converted tabs to spaces

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ var HELP = argv.help || null;
 
 // help if asked
 if (HELP) {
-	console.log('--startfrom <number>','\t','(optional) Set bookmark to <number>. First line is 0. Will % at poem length. Empty --startfrom will reset to 0.');
-	process.exit();
+    console.log('--startfrom <number>','\t','(optional) Set bookmark to <number>. First line is 0. Will % at poem length. Empty --startfrom will reset to 0.');
+    process.exit();
 }
 
 // 1. check bookmark & read poem
@@ -28,42 +28,42 @@ if (HELP) {
 // 3. update bookmark
 // 4. tweet line
 async.parallel([
-	checkBookmark,
-	function (done) { fs.readFile(_POEM_FILE, 'utf8', done); }
+    checkBookmark,
+    function (done) { fs.readFile(_POEM_FILE, 'utf8', done); }
 ], function (err, results) {
-	if (err) console.log(err);
-	var bookmark = results[0];
-	var poem = results[1].split('\n');
-	var line = poem[bookmark % poem.length];
-	var new_bookmark = (bookmark + 1) % poem.length;
-	fs.writeFile(_BOOKMARK_FILE, new_bookmark+'\n', 'utf8', function(err) {
-		if (err) throw err;
-		tweetMessage(line, function(data) {
-			console.log('tweeted line', bookmark, ':', data.text, 'at', data.created_at);
-	    });
-	});
+    if (err) console.log(err);
+    var bookmark = results[0];
+    var poem = results[1].split('\n');
+    var line = poem[bookmark % poem.length];
+    var new_bookmark = (bookmark + 1) % poem.length;
+    fs.writeFile(_BOOKMARK_FILE, new_bookmark+'\n', 'utf8', function(err) {
+        if (err) throw err;
+        tweetMessage(line, function(data) {
+            console.log('tweeted line', bookmark, ':', data.text, 'at', data.created_at);
+        });
+    });
 });
 
 function checkBookmark(callback) {
-	if (START_FROM === null) {
-		fs.exists(_BOOKMARK_FILE, function (exists) {
-			if (exists) {
-				fs.readFile(_BOOKMARK_FILE, 'utf8', function(err, data) {
-					if (data === null) {
-						callback(null, 0);
-					} else {
-						callback(null, parseInt(data.trim()));
-					}
-				});
-			} else {
-				callback(null, 0);
-			}
-		});		
-	} else {
-		callback(null, START_FROM);
-	}
+    if (START_FROM === null) {
+        fs.exists(_BOOKMARK_FILE, function (exists) {
+            if (exists) {
+                fs.readFile(_BOOKMARK_FILE, 'utf8', function(err, data) {
+                    if (data === null) {
+                        callback(null, 0);
+                    } else {
+                        callback(null, parseInt(data.trim()));
+                    }
+                });
+            } else {
+                callback(null, 0);
+            }
+        });     
+    } else {
+        callback(null, START_FROM);
+    }
 }
 
 function tweetMessage(message, callback) {
-	_TWIT.updateStatus(message, callback);
+    _TWIT.updateStatus(message, callback);
 }
